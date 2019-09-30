c278708dd92a4dade4790e0d44b7f55837c86ba9
Eugene Farafonov <eu.farafonov@gmail.com>
2019-09-30 14:42:11 +0300

    init: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 != 0) return false; 
+  
+  let result = [];
+  
+  str.split("").forEach(function(symb) {
+    bracketsConfig.forEach(function(el, i, arr) {
+      let ind = el.indexOf(symb);
+      
+      if (ind == 0) {
+        if (symb == el[1] && symb == result[result.length - 1]) {
+          result.pop();
+        } else {
+          result.push(symb);
+        }
+      }
+      
+      if (ind == 1) {
+        if (result.length == 0) return false;
+
+        if (result[result.length - 1] == el[0]) {
+          result.pop();
+        }
+      }
+    });
+    
+  });
+  
+  return (result.length == 0) ? true : false;
 }
